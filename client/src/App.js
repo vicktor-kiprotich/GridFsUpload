db529fd4978153bdac528c26a61b5f668b99b4de
Devvicktor <vkosgei001@gmail.com>
2020-09-13 17:24:02 +0300

    changed the client's file structure

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">GridFs image upload React app</header>
+      <p>upload files and see them</p>
+    </div>
+  );
+}
+
+export default App;
